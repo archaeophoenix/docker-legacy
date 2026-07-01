f0899e99bf93454233bd94bce7bf21964cdfd781
ARCHAEOPHOENIX <hachmadafif@gmail.com>
2026-07-01 13:48:56 +0700

    repair node

--- node/docker-entrypoint.sh
+++ node/docker-entrypoint.sh
@@ -1,18 +1,6 @@
 #!/bin/sh
+set -e
 
 echo "Starting Node container..."
 
-# install dependencies jika belum ada
-if [ ! -d "node_modules" ]; then
-    echo "Installing dependencies..."
-    yarn install
-fi
-
-# generate vite project jika belum ada
-if [ ! -f "package.json" ]; then
-    echo "No package.json found!"
-    exit 1
-fi
-
-# start vite
-yarn dev --host
+exec tail -f /dev/null
